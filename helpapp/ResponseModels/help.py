3c6a2d46cc3c5976da5034cd587f7a4ce8f3c6ac
biker9124 <brownjs193@gmail.com>
2021-11-15 16:48:21 -0500

    Cleaned up json being sent to client. Cleaned up response model for json.

---
+++ helpapp/ResponseModels/help.py
@@ -0,0 +1,19 @@
+class Help(object):
+    def __init__(self, description: str, analysis_results: dict, location: str, resources: dict = None):
+
+        self.description = description
+        self.resources = resources
+        self.analysis_results = analysis_results
+        self.location = location
+
+    def to_dict(self):
+        help_dict = {"resources": self.resources, "location": self.location,
+                     "description": self.description, "analysisResults": self.analysis_results}
+        return help_dict
+
+    def __repr__(self):
+        return(f'Conflict(\
+            resources = {self.resources}, \
+                description = {self.description}, \
+                        description = {self.location}, \
+                   analysisResults = {self.analysis_results},)')
--- helpapp/views.py
+++ helpapp/views.py
@@ -15,7 +15,7 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from .firestore import db
 import json
-from .ResponseModels.conflict import Conflict
+from .ResponseModels.help import Help
 from .ResponseModels.location import Location
 from .ResponseModels.user import User
 from .Providers.analysis import Analysis
@@ -23,29 +23,30 @@ from .Providers.analysis import Analysis
 import uuid
 
 
-
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
 @csrf_exempt
 def user(request):
-    #register user
+    # register user
     if request.method == 'POST':
-        json_data = json.loads(request.body)
         
+        json_data = json.loads(request.body)
+
         user_id = str(uuid.uuid4())
         doc_ref = db.collection(u'user').document()
-        analysis = Analysis( description=json_data['description']  )
+        
+        analysis = Analysis(description=json_data['description'])
         analysis_results = analysis.results()
-        conflict = Conflict(type=analysis_results['potential_causes'],description=json_data['description'] ,
-        suicidal=analysis_results['is_suicide'])
-        conflict_resources = []
-        
-        
-        
+        help_response = Help(analysis_results=analysis_results, description=json_data['description'], location=json_data['location']
+                             )
+
         for key, value in analysis_results['potential_causes'].items():
-            collections = db.collection('conflict_resources').document(value).get()
-            conflict_resources.append(collections._data)
-        
+            collections = db.collection(
+                'conflict_resources').document(value).get()
+            help_response.resources = collections._data
+        response = help_response.to_dict()
         # doc_ref.set(User(json_data['name'], user_id, json_data['location'], conflict.to_dict()).to_dict())
-        return JsonResponse({'name':json_data['name'], 'analysis_results':analysis_results})
+        return JsonResponse(response)
